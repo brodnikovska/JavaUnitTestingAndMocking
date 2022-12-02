68f3ee0664adfb35615d2e292e3b1f947d080512
Brodnikovska <brodni01@kits.kfplc.com>
2022-12-02 17:37:03 +0200

    Fux logic

--- JavaUnitTesting/src/main/java/test/TestShoppingCart.java
+++ JavaUnitTesting/src/main/java/test/TestShoppingCart.java
@@ -22,73 +22,85 @@ public class TestShoppingCart {
     }
 
     @Test
-    public void testAddProductToCartSmokeHappyPath() {
+    public void testAddProductToCartProductNotFound() {
         List<Product> shoppingList = new ArrayList<>();
         shoppingList.add(potato);
-        shoppingList.add(carrot);
         ShoppingCart shoppingCart = new ShoppingCart(shoppingList);
 
         shoppingCart.addProductToCart(meat);
 
-        Assertions.assertEquals(shoppingCart.getProducts().size(), 3);
-        Assertions.assertTrue(shoppingCart.getProductById(meat.getId()).equals(meat));
+        Assertions.assertEquals(2, shoppingCart.getProducts().size());
+        Assertions.assertEquals(1.5, shoppingCart.getProductById(meat.getId()).getQuantity());
     }
 
     @Test
-    public void testAddProductToCartOneProductAndOneEmptyProduct() {
+    public void testAddProductToCartTwoExistingProductIsFound() {
         List<Product> shoppingList = new ArrayList<>();
-        shoppingList.add(new Product());
-        ShoppingCart shoppingCart = new ShoppingCart(shoppingList);
-
-        shoppingCart.addProductToCart(potato);
-
-        Assertions.assertEquals(shoppingCart.getProducts().size(), 2);
-        Assertions.assertTrue(shoppingCart.getProductById(potato.getId()).equals(potato));
-    }
-
-    @Test
-    public void testRemoveProductToCartSmokeHappyPath() {
-        List<Product> shoppingList = new ArrayList<>();
-        shoppingList.add(potato);
-        shoppingList.add(carrot);
         shoppingList.add(meat);
         ShoppingCart shoppingCart = new ShoppingCart(shoppingList);
 
-        shoppingCart.removeProductFromCart(potato);
-        Assertions.assertEquals(shoppingCart.getProducts().size(), 2);
-        Assertions.assertThrows(ProductNotFoundException.class, () -> shoppingCart.getProductById(potato.getId()));
+        shoppingCart.addProductToCart(meat);
+
+        Assertions.assertEquals(1, shoppingCart.getProducts().size());
+        Assertions.assertEquals(3, shoppingCart.getProductById(meat.getId()).getQuantity());
     }
 
     @Test
-    public void testRemoveProductToCartOneProductAndOneEmptyProduct() {
+    public void testRemoveProductFromCartProductNotFound() {
+        List<Product> shoppingList = new ArrayList<>();
+        shoppingList.add(potato);
+        shoppingList.add(carrot);
+        ShoppingCart shoppingCart = new ShoppingCart(shoppingList);
+
+        try {
+            shoppingCart.removeProductFromCart(meat);
+        } catch (ProductNotFoundException exception) {
+            Assertions.assertThrows(ProductNotFoundException.class, () -> shoppingCart.getProductById(meat.getId()));
+        }
+        Assertions.assertEquals(2, shoppingCart.getProducts().size());
+
+    }
+
+    @Test
+    public void testRemoveProductFromCartProductIsFound() {
         List<Product> shoppingList = new ArrayList<>();
         shoppingList.add(new Product());
         shoppingList.add(carrot);
         ShoppingCart shoppingCart = new ShoppingCart(shoppingList);
 
         shoppingCart.removeProductFromCart(carrot);
-        Assertions.assertEquals(shoppingCart.getProducts().size(), 1);
-        Assertions.assertThrows(ProductNotFoundException.class, () -> shoppingCart.getProductById(carrot.getId()));
+        Assertions.assertEquals(1, shoppingCart.getProducts().size());
+        Assertions.assertEquals(0, shoppingCart.getCartTotalPrice());
     }
 
     @Test
-    public void testGetCartTotalPriceSmokeHappyPath() {
+    public void testGetProductByIdProductNotFound() {
+        List<Product> shoppingList = new ArrayList<>();
+        shoppingList.add(meat);
+        shoppingList.add(carrot);
+        ShoppingCart shoppingCart = new ShoppingCart(shoppingList);
+
+        Assertions.assertThrows(ProductNotFoundException.class, () -> shoppingCart.getProductById(potato.getId()));
+    }
+
+    @Test
+    public void testGetProductByIdProductIsFound() {
+        List<Product> shoppingList = new ArrayList<>();
+        shoppingList.add(meat);
+        shoppingList.add(carrot);
+        ShoppingCart shoppingCart = new ShoppingCart(shoppingList);
+
+        Assertions.assertEquals(meat, shoppingCart.getProductById(3));
+    }
+
+    @Test
+    public void testGetCartTotalPrice() {
         List<Product> shoppingList = new ArrayList<>();
         shoppingList.add(potato);
         shoppingList.add(carrot);
-        shoppingList.add(meat);
-        ShoppingCart shoppingCart = new ShoppingCart(shoppingList);
-
-        Assertions.assertEquals(shoppingCart.getCartTotalPrice(), 12.815, 0.0001);
-    }
-
-    @Test
-    public void testGetCartTotalPriceOneProductAndOneEmptyProduct() {
-        List<Product> shoppingList = new ArrayList<>();
         shoppingList.add(new Product());
-        shoppingList.add(meat);
         ShoppingCart shoppingCart = new ShoppingCart(shoppingList);
 
-        Assertions.assertEquals(shoppingCart.getCartTotalPrice(), 7.755, 0.0001);
+        Assertions.assertEquals(5.06, shoppingCart.getCartTotalPrice(), 0.0001);
     }
 }
